a8fc3f44987ef245a6d911e1e657895b9a67634e
John Lee <jlee14c@hotmail.com>
2019-05-04 15:10:14 -0700

    screens

---
+++ app/src/main/java/com/vaxezy/johnlee/vaxezy/recommendation/RecommendationActivity.java
@@ -0,0 +1,16 @@
+package com.vaxezy.johnlee.vaxezy.recommendation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vaxezy.johnlee.vaxezy.R;
+
+public class RecommendationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendation);
+    }
+
+}
