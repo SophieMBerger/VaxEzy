34fb1299d605599994291380aa28ec376e3153ac
Sophie <berger.m.sophie@gmail.com>
2019-05-04 14:16:59 -0700

    minor changes

--- app/src/main/java/com/vaxezy/johnlee/vaxezy/homeactivity/cameraActivity.java
+++ app/src/main/java/com/vaxezy/johnlee/vaxezy/homeactivity/cameraActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -60,5 +61,7 @@ public class cameraActivity extends AppCompatActivity {
             imageView.setImageBitmap(photo);
         }
     }
+
+
 }
 
